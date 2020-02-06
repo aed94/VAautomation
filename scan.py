02638f7e651554398e174f0008eae87e77910fb1
aed94 <38212739+aed94@users.noreply.github.com>
2020-02-06 15:09:17 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -11,13 +11,19 @@ urllib3.disable_warnings()
 def head(url):
 	print("Beginning scan on %s "%(url))
 	print(" ")
+	# r = requests.get(url)
+	# print("Scan done..")
+	# return r.headers
 
 def get_request(url):
 	r = requests.get(url)
 	print("Testing information disclosure through response headers")
+	# response = r.text
+	# print(r.headers)
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False
 
 def post_request(url):
@@ -27,6 +33,7 @@ def post_request(url):
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False
 
 def head_request(url):
@@ -36,6 +43,7 @@ def head_request(url):
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False		
 
 def options_request(url):
@@ -45,6 +53,7 @@ def options_request(url):
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False
 
 def trace_request(url):
@@ -54,15 +63,17 @@ def trace_request(url):
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False
 
 def put_request(url):
-	r = requests.trace(url)
+	r = requests.put(url)
 	print("Testing information disclosure through response headers")
 	response = r.text
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False
 
 def delete_request(url):
@@ -72,15 +83,40 @@ def delete_request(url):
 	if "Server" or "X-Powered-By" in r.text:
 		return True
 	else:
+		# print("No Server or X-Powered-By headers found..")
 		return False
+
+def request_methods():
+	methods = ["GET", "POST", "HEAD", "TRACE", "OPTIONS", "PUT", "DELETE"]
+	for i in methods:
+		if (get_request(url) == True):
+			break
+		elif (post_request(url) == True):
+			break
+		elif (head_request(url) == True):
+			break
+		elif (options_request(url) == True):
+			break
+		elif (trace_request(url) == True):
+			break
+		elif (put_request(url) == True):
+			break
+		elif (delete_request(url) == True):
+			break
+		else:
+			print("Something went wrong.")
+			break	
+	print("\nApplication discloses information in Server: or X-Powered-By: header via %s method\n") % (i) 	
+
 
 def cookie_attributes(url):
 	# headers = {"Cookie": "JSESSIONID=F0DC6310EE44C69E712B9E703CB3E724"}
 	# r = requests.post(url, headers = headers)
 	r = requests.get(url)
-
+	# print(r.headers)
+	print(r.cookies)
 	if 'Secure' not in r.headers['Set-Cookie']:
-		print("Secure Flag not set\n")
+		print ("Secure Flag not set\n")
 		print(r.headers['Set-Cookie'])
 		print("\n")
 	if 'HttpOnly' not in r.headers['Set-Cookie']:
@@ -92,43 +128,47 @@ def cookie_attributes(url):
 		print(r.headers['Set-Cookie'])
 		print("\n")
 
+	# if cookie.domain 
+	# print(r.text)
+	# if 'Admin' in r.text:
+	# print(r.headers['Set-Cookie'])
 	for cookie in r.cookies:
 		if cookie.domain not in url:
 			print("Domain of Session Cookie is not set appropriately")
 
-def strict_http(url):
-	if 'https' in url and 'Strict-Transport-Security' not in r.headers:
-		print("Missing Strict-Transport-Security headers\n")
+def hsts_header(url):
+  	#if 'http' in url and r.headers['Strict-Transport-Security'] not in r.headers:
+  	if 'https' in url and 'Strict-Transport-Security' not in r.headers:
+  		print("Strict-Transport-Security header not set")
 
-def content_sec_policy(url):
+def content_security_policy(url):
 	if 'Content-Security-Policy' not in r.headers:
-		print("Missing Content-Security-Policy header missing")
+		print("Content-Security-Policy header not set\n")
 
-url = "https://old.axismf.com/"
-cookies = {"ASP.NET_SessionId":"c443xf3tdtqock2a1024oxxd"}
-r = requests.get(url, cookies=cookies)
-head(url)
-#cookie_attributes(url)
-strict_http(url)
-content_sec_policy(url)
+def content_type(url):
+	if 'X-Content-Type-Options' not in r.headers:
+		print("Missing X-Content-Type-Options header\n")
 
-methods = ["GET", "POST", "HEAD", "TRACE", "OPTIONS", "PUT", "DELETE"]
-for i in methods:
-	if (get_request(url) == True):
-		break
-	elif (post_request(url) == True):
-		break
-	elif (head_request(url) == True):
-		break
-	elif (options_request(url) == True):
-		break
-	elif (trace_request(url) == True):
-		break
-	elif (put_request(url) == True):
-		break
-	elif (delete_request(url) == True):
-		break
-	else:
-		print("Something went wrong.")
-		break	
-print("\nApplication discloses information in Server: or X-Powered-By: header via %s method") % (i) 	
+def cache_control(url):
+	cc = ['no-cache', 'no-store', 'must-revalidate','max-age']
+	if 'Cache-Control' not in r.headers:
+		print("Cache-control header missing")
+	elif cc not in r.headers['Cache-Control']:
+			print("Cache-Control header not configured correctly")
+
+	
+
+url = "http://demo.testfire.net/"
+r = requests.get(url)
+
+if __name__ == '__main__':
+	print(r.headers)
+	head(url)
+	request_methods()
+	cookie_attributes(url)
+	hsts_header(url)
+	content_security_policy(url)
+	content_type(url)
+	cache_control(url)
+
+print("End")
